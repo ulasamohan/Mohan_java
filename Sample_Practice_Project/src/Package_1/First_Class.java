c8d5469f1d08a260d8d65695cd178f0b6c2e820b
ulasamohan <77103848+ulasamohan@users.noreply.github.com>
2021-03-04 22:52:12 +0530

    Mohan_first_program_java

---
+++ Sample_Practice_Project/src/Package_1/First_Class.java
@@ -0,0 +1,13 @@
+package Package_1;
+
+public class First_Class {
+
+	public static void main(String[] args) {
+		System.out.println("This is mohan");
+		
+		System.out.println(10*20+"mohan");
+		System.out.println("mohan"+10*20);
+
+	}
+
+}
